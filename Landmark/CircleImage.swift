a09a4701ba196678b0704b7e76f6e3479d4df006
omofolarin <folarinshonibare@yahoo.com>
2022-07-10 21:05:32 +0100

    creating and combining views

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by Shonibare Omofolarin Ajibade on 10/07/2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
