217dec891e15e5d416ed89f89cafc1205ba59814
omofolarin <folarinshonibare@yahoo.com>
2022-07-10 15:59:14 +0100

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Shonibare Omofolarin Ajibade on 10/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
