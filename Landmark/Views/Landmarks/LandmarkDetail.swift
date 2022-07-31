6a3cc42de64dcec5832d29683ffbe1b2dd8383f5
omofolarin <folarinshonibare@yahoo.com>
2022-07-31 06:19:55 +0100

    states and user input

--- Landmark/Views/Landmarks/LandmarkDetail.swift
+++ Landmark/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    }
     
     var body: some View {
         ScrollView {
@@ -21,15 +26,17 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                    Text("Turtle Rock ")
-                        .font(.title)
-                        
-                
                     HStack {
-                        Text("Joshua Tree National Park")
+                        Text(landmark.name)
+                                .font(.title)
+                        FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                    }
+            
+                    HStack {
+                        Text(landmark.park)
                             .font(.subheadline)
                         Spacer()
-                        Text("Califonia")
+                        Text(landmark.state)
                             .font(.subheadline)
                     }
                     .font(.subheadline)
@@ -50,7 +57,11 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static var modelData = ModelData()
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        Group {
+            LandmarkDetail(landmark: modelData.landmarks[0]).environmentObject(modelData)
+        }.previewLayout(.fixed(width: 200, height: 70))
+       
     }
 }
